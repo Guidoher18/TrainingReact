5ef55619aaab507b0a127b111d13b23056bf8292
Guido Caruso <guido.caruso@flockit.com.ar>
2025-07-23 12:57:49 -0300

    Render Users by data list

--- CURSO_REACT_2024_MIDULIVE/first-app-vite/src/main.tsx
+++ CURSO_REACT_2024_MIDULIVE/first-app-vite/src/main.tsx
@@ -4,22 +4,41 @@ import './preset.css'
 import './index.css'
 import { TwitterCard } from './Components/TwitterCard'
 
+const data = [
+  {
+    avatarUrl: 'https://avatar.iran.liara.run/public/42',
+    account: '@guido',
+    name: 'Guido',
+    isFollowing: true
+  },
+  {
+    avatarUrl: 'https://avatar.iran.liara.run/public/43',
+    account: '@jDoe',
+    name: 'John Doe',
+    isFollowing: false
+  },
+  {
+    avatarUrl: 'https://avatar.iran.liara.run/public/44',
+    account: '@AnnZ',
+    name: 'Ann Z',
+    isFollowing: true
+  },
+];
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <h3><strong>A quién seguir</strong></h3>
-    <TwitterCard
-      avatarUrl='https://avatar.iran.liara.run/public/42'
-      account='@guido'
-      initialIsFollowing
-    >
-      Guido
-    </TwitterCard>
 
-    <TwitterCard
-      avatarUrl='https://avatar.iran.liara.run/public/42'
-      account='@jDoe'
-    >
-      John Doe
-    </TwitterCard>
+    { 
+      data.map(({avatarUrl, account, name, isFollowing}) => (
+        <TwitterCard
+          avatarUrl={avatarUrl}
+          account={account}
+          initialIsFollowing={isFollowing}
+        >
+          {name}
+        </TwitterCard>
+      ))
+    }
   </StrictMode>,
 )
